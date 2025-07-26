044f8f896ee09821e4de359c9fc2784ae99392ea
Aura Cichoń <4654533+vddCore@users.noreply.github.com>
2025-07-26 10:36:53 +0200

    Initial commit

---
+++ GlitoneaDesktopApp/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using Avalonia;
+global using Avalonia.Controls;
+global using Avalonia.Controls.ApplicationLifetimes;
+global using Avalonia.Markup.Xaml;
+
+global using Glitonea.Mvvm;
+global using Glitonea.UI;
+global using Glitonea.Persistence;
+global using Glitonea.Persistence.Json;
+
+global using PropertyChanged;
+global using _DependsOnAttribute = Avalonia.Metadata.DependsOnAttribute;
---
+++ GlitoneaDesktopApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GlitoneaDesktopApp;
+
+using System;
+using System.Reflection;
+using Avalonia;
+using Glitonea;
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+        => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UseGlitoneaFramework(Assembly.GetExecutingAssembly())
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
